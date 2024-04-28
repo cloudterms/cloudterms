5f8f9cce434c8f75b73f2841c28cedf1c2290190
Jason Atallah <jasonatallah@gmail.com>
2024-04-28 13:41:12 -0500

    wip

---
+++ scripts/publish-all.ts
@@ -0,0 +1,11 @@
+import { $ } from "bun";
+
+const packages = (await $`ls packages`).text();
+
+for (const pkg of packages.split("\n")) {
+  if (pkg) {
+    await $`cd packages/${pkg} && bun run publish`;
+  } else {
+    console.log(pkg, "not found");
+  }
+}
