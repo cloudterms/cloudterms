00598302d7ec77dd20a8d46204abe8dd5d5c1543
Jason Atallah <jasonatallah@gmail.com>
2024-05-04 16:48:15 -0500

    repo cleanup

--- examples/hono-api/src/index.ts
+++ examples/hono-api/src/index.ts
@@ -8,16 +8,19 @@ const cloudterms = CloudTerms();
 
 app.use("/*", cors());
 
+// Route to pull your applications terms from CloudTerms
 app.get("/terms", async (c) => {
   const terms = await cloudterms.terms.get();
   return c.json({ terms });
 });
 
+// Route to check if a user has agreed to your terms
 app.get("/user/:userId/has-agreed", async (c) => {
   const hasAgreed = await cloudterms.user.hasAgreed(c.req.param("userId"));
   return c.json({ hasAgreed });
 });
 
+// Route to set a user as agreed to your terms
 app.post("/user/:userId/agree", async (c) => {
   const setAgreed = await cloudterms.user.setAgreed(c.req.param("userId"));
   return c.json({ setAgreed });
